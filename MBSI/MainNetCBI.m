f20c6439033544a7c372f21cf8a69e935f3051d5
huayu <yuhua200886@163.com>
2016-10-25 10:03:54 +0800

    Delete MainNetCBI.m

--- MBSI/MainNetCBI.m
+++
@@ -1,8 +0,0 @@
-function [CandiRankListNetCBI]=MainNetCBI()
-clear all; clc;
-%== setting initilize program input parameters
-DataPath='G:\WorkDir\NetCBI\Data\Version2010\';
-DiseaseName='Breast Neoplasms';
-% [FinalCandiRankList,AUC] = LeaveOneOutCrossValidationNetCBI(DataPath);
-CandiRankListNetCBI=PredictNetCBI(DataPath,DiseaseName);
-end
