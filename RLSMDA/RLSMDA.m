5b84a4c8248341e5eace56a986bec9cd11883ec0
huayu <yuhua200886@163.com>
2016-11-28 20:59:14 +0800

    Add files via upload

---
+++ RLSMDA/RLSMDA.m
@@ -0,0 +1,19 @@
+function [CandiRankList] = RLSMDA(candiMiRNAs,miRNA_name,MiFunSim,DiPheSim,mi2diNetwork,DiseaseIndex)
+w=0.1;
+CandiValueList=zeros(length(candiMiRNAs),1);
+[MnRow,~] = size(MiFunSim);
+MIMatrix = eye(MnRow);
+[DnRow,~] = size(DiPheSim);
+DIMatrix = eye(DnRow);
+Mres = MiFunSim*(MiFunSim+(MiFunSim*MIMatrix))*mi2diNetwork;
+Dres = DiPheSim*(DiPheSim+(DiPheSim*DIMatrix))*(mi2diNetwork');
+OptRes = w*Mres+ (1-w)*Dres';
+for i=1:length(candiMiRNAs)
+    LogicIndex = strcmp(candiMiRNAs{i},miRNA_name);
+    matchIndex = find(LogicIndex,1);
+    CandiValueList(i) = OptRes(matchIndex,DiseaseIndex);
+end
+CandiRankList(:,1)=candiMiRNAs;
+CandiRankList(:,3)=num2cell(CandiValueList);
+end
+
