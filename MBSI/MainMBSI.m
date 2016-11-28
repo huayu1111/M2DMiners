dc3cc729be7f82baddd4a2fd6a9a3fca93a7acda
huayu <yuhua200886@163.com>
2016-11-28 11:11:11 +0800

    Delete MainMBSI.m

--- MBSI/MainMBSI.m
+++
@@ -1,8 +0,0 @@
-function [CandiRankListMBSI]=MainMBSI()
-clear all; clc;
-%== setting initilize program input parameters
-DataPath='G:\WorkDir\MBSI\Data\Version2010\';
-DiseaseName='Breast Neoplasms';
-% [FinalCandiRankList,AUC] = LeaveOneOutCrossValidationMBSI(DataPath);
-CandiRankListMBSI=PredictMBSI(DataPath,DiseaseName);
-end
