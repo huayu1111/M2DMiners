0adf1f0e355df6f751dc5f242a35524b5342137a
huayu <yuhua200886@163.com>
2016-11-30 14:54:36 +0800

    Update ConBooleanPhe.m

--- JiangsMethod/ConBooleanPhe.m
+++ JiangsMethod/ConBooleanPhe.m
@@ -1,6 +1,6 @@
 function [BooleanPheNetwork] = ConBooleanPhe(DataPath)
 load([DataPath,'DiPheSim.mat']);
-[DiRow,DiCol,~]=find(DiPheSim>=0.2);
+[DiRow,DiCol,~]=find(DiPheSim>=0.3);
 BooleanPheNetwork=zeros(length(DiPheSim),length(DiPheSim));
 for i=1:length(DiRow)
     BooleanPheNetwork(DiRow(i),DiCol(i))=1;
